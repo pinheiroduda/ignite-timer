ab903ebb8ed61b906e5f206bf10ff563e07c0529
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-10-13 17:02:20 -0300

    feat: adds react router dom and firsts app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Router />
+
       <GlobalStyle />
     </ThemeProvider>
   )
