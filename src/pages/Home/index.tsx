023590d537eea7c2903f3eddf98801eb5be85a36
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-10-18 21:40:05 -0300

    feat: adds functionality to watch changes and deal with form submit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+
 import {
   CountDownContainer,
   FormContainer,
@@ -10,15 +12,24 @@ import {
 } from './styles'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -36,6 +47,7 @@ export function Home() {
             step="5"
             min="0"
             max="60"
+            {...(register('minutesAmount'), { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -49,7 +61,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDownButton type="submit">
+        <StartCountDownButton disabled={!task} type="submit">
           <Play size={24} />
           Começar
         </StartCountDownButton>
