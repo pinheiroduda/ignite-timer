506610fe13361f5ddd573c73f381da1d867c7cad
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-11-02 23:46:47 -0300

    feat: save cycles in local storage

--- src/pages/Home/components/CountDown/index.tsx
+++ src/pages/Home/components/CountDown/index.tsx
@@ -34,7 +34,7 @@ export function CountDown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondsDifference >= totalSeconds) {
